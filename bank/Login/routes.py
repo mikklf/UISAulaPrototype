9c1d7d1c986ffb4051340524dd218bdc7135f9b5
NikolajDanger <nikolajgade@live.dk>
2022-05-24 14:16:44 +0200

    :broom: small cleanup

--- bank/Login/routes.py
+++ bank/Login/routes.py
@@ -1,8 +1,8 @@
 from flask import render_template, url_for, flash, redirect, request, Blueprint
-from bank import app, conn, bcrypt
+from bank import bcrypt
 from bank.forms import CustomerLoginForm, EmployeeLoginForm
 from flask_login import login_user, current_user, logout_user, login_required
-from bank.models import Customers, select_Customers, select_Employees
+from bank.models import select_Customers, select_Employees
 
 Login = Blueprint('Login', __name__)
 
