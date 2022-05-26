0499d394d8b54b8a929ebe9b1dde466543492e54
Asger <wqm216@ku.dk>
2022-05-26 12:49:19 +0200

    Schema init changes

--- aula/schema_ins.sql
+++ aula/schema_ins.sql
@@ -8,14 +8,14 @@ INSERT INTO public.users(user_id, first_name, last_name, password, email, addres
     (5004,'Phoebe', 'Buffay', '$2b$12$KFkp1IEMGT4QrWwjPGhE3ejOv6Z3pYhx/S4qOoFbanR2sMiZqgeJO', 'phoebe@test.dk', 'Central Perk', 'parent'),
     (5005,'Ross', 'Geller', '$2b$12$KFkp1IEMGT4QrWwjPGhE3ejOv6Z3pYhx/S4qOoFbanR2sMiZqgeJO', 'ross@test.dk', 'Central Perk', 'student');
 
-INSERT INTO public.groups(group_id, name, leaveable, parents_can_post) VALUES 
-    (1000, 'fodbold', TRUE, TRUE), 
-    (1001,'3a', FALSE, TRUE);
+INSERT INTO public.groups(group_id, name, hidden) VALUES 
+    (1000, 'fodbold', TRUE), 
+    (1001,'3a', FALSE);
 
 INSERT INTO users_groups (user_id, group_id) VALUES (5000, 1000), (5001, 1001);
 
-INSERT INTO threads(thread_id ,title, group_id, creator_id) VALUES 
-    (2000, 'Anbefalinger til fodboldsko?', 1000, 5000);
+INSERT INTO threads(thread_id ,title, group_id) VALUES 
+    (2000, 'Anbefalinger til fodboldsko?', 1000);
 
 INSERT INTO messages (message_id, content, thread_id, author_id) VALUES (4000,'Eleverne har ondt i fødderne når de spiller fodbild. Hvem kender et godt skomærke?', 2000, 5000);
 
